5d913e77b3d1940c085f713fc300229d7e7eaa13
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-04-07 12:36:04 -0300

    Added the feature to let the user choose the number of cards to play

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,11 @@ function rulesWindow() {
     rules.classList.toggle("show_rules");
 }
 
-function totalCards() {
+function initGame() {
     
-    const buttons = document.querySelector(".buttons");
-    
-    
-    let number = 0;
+    let number = 0; // N° de cartas que o jogador escolher
+    const buttons = document.querySelector(".buttons"); // tag que engloba os botões Iniciar e Regras
+    const board = document.querySelector("main .game"); // Tela inicial do jogo (sem cartas)
     
     
     
@@ -17,6 +16,18 @@ function totalCards() {
     } while(number < 4 || number > 14 || number%2 !== 0 || number === null)
 
     buttons.classList.add("hide_buttons");
+
+    for (let i = 0; i < number; i++) {
+        const card = document.createElement("li");
+        const img = document.createElement("img");
+        
+        img.setAttribute("src","./images/front.png");
+        card.className = "card";
+        
+        card.appendChild(img);
+        board.appendChild(card);
+    }
+
 
 }
 
