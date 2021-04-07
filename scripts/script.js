afecf622a95f944cda3eb6ee818661f261a190e9
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-04-07 01:22:02 -0300

    Added the prompt to begin the game

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,22 @@
-function rulesWindow(){
+function rulesWindow() {
     const rules = document.querySelector("main .rules_content");
     rules.classList.toggle("show_rules");
 }
+
+function totalCards() {
+    
+    const buttons = document.querySelector(".buttons");
+    
+    
+    let number = 0;
+    
+    
+    
+    do{
+        number = Number(prompt("Digite o número de cartas: "));
+    } while(number < 4 || number > 14 || number%2 !== 0 || number === null)
+
+    buttons.classList.add("hide_buttons");
+
+}
+
