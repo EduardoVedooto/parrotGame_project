8c1c2f0f4d815c0c94fe13046a524242359e0c5d
Eduardo Vedooto <vedootoeduardo@gmail.com>
2021-04-08 23:19:41 -0300

    Added the game logic but have some bugs yet/ Added seconds counter

--- scripts/script.js
+++ scripts/script.js
@@ -8,11 +8,15 @@ const imagesName = ["bobrossparrot.gif",
                     ];
 
 let imagesArray = [];
-
+let card1;
+let card2;
+let seconds = 0;
 
 function rulesWindow() {
     const rules = document.querySelector("main .rules_content");
+    const header = document.querySelector("header");
     rules.classList.toggle("show_rules");
+    header.classList.toggle("hide_header");
 }
 
 function initGame() {
@@ -20,7 +24,8 @@ function initGame() {
     let number = 0; // N° de cartas que o jogador escolher
     const buttons = document.querySelector(".buttons"); // tag que engloba os botões Iniciar e Regras
     const board = document.querySelector("main .game"); // Tela inicial do jogo (sem cartas)
-    
+    const clock = document.querySelector("main .clock"); // Relógio que conta segundos
+    const secs = document.querySelector("main .clock .secs"); // Div segundos
     
     do{
         number = Number(prompt("Digite o número de cartas: "));
@@ -41,17 +46,19 @@ function initGame() {
     
 
     for (let i = 0; i < number; i++) {
-        createCard(number/2);    
+        createCard();    
     }
 
-
+    secs.innerHTML = seconds;
+    clock.classList.add("display_clock");
+    setInterval(countSeconds, 1000, secs);
 
     board.classList.add("init_game");
 }
 
 
 // O parâmetro desta função servirá para saber quantos pares de gifs terei que pegar do array "images"
-function createCard(numberOfGIFs){
+function createCard(){
     const board = document.querySelector("main .game");
     const card = document.createElement("li");
     const cardContent = document.createElement("div");
@@ -59,9 +66,8 @@ function createCard(numberOfGIFs){
     const cardBackFace = document.createElement("div");
     const frontImg = document.createElement("img");
     const backImg = document.createElement("img");
-    const nameGIF = imagesArray[imagesArray.length - 1];
-    imagesArray.pop();
-    console.log(imagesArray);
+    const GIFName = imagesArray[imagesArray.length - 1];
+    
 
     card.className = "card";
     
@@ -77,7 +83,7 @@ function createCard(numberOfGIFs){
 
     frontImg.setAttribute("src", "./images/front.png");
     
-    backImg.setAttribute("src", `./images/${nameGIF}`);
+    backImg.setAttribute("src", `./images/${GIFName}`);
 
     cardFrontFace.appendChild(frontImg);
     cardBackFace.appendChild(backImg);
@@ -88,9 +94,44 @@ function createCard(numberOfGIFs){
     card.appendChild(cardContent);
     
     board.appendChild(card);
+    imagesArray.pop();
 }
 
 function turnCard(card){
     
-    card.classList.toggle("isFlipped");
+    card.classList.add("isFlipped")
+    
+    
+    const cards = document.querySelectorAll(".isFlipped");
+
+
+    if(cards.length === 2){
+        if(cards[0].children[1].children[0].getAttribute("src") !== cards[1].children[1].children[0].getAttribute("src")){
+            setTimeout(turnBackCard, 1000, cards);
+        } else {
+            pairFound(cards);
+        }
+    }
+
+    console.log(cards);
+    console.log(card.children[1].children[0].getAttribute("src"));
+
+    
+}
+
+function turnBackCard(cards){
+    cards[0].classList.remove("isFlipped");
+    cards[1].classList.remove("isFlipped");
+}
+
+function countSeconds(secs){
+    secs.innerHTML = seconds;
+    seconds++;
+}
+
+function pairFound(cards){
+    cards[0].classList.remove("isFlipped");
+    cards[0].classList.add("found");
+    cards[1].classList.remove("isFlipped");
+    cards[1].classList.add("found");
 }
